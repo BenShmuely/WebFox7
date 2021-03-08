12083b7aa824452b45646f8de097f2f2f4f1af15
benshmuely <ben.shmuely@whitesourcesoftware.com>
2021-03-08 18:01:11 +0200

    first commit

---
+++ Controllers/SecureCookie/SecureCookieTest1.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebFox.Controllers
+{
+    public class SecureCookieTest1: ControllerBase
+    {
+        [HttpGet("{response}")]
+        [HttpGet("{request}")]
+
+        // HttpCookie myCookie = new HttpCookie("Sensitive cookie");
+
+        public void Unsafe(HttpResponse response, HttpRequest request)
+        {
+            var options = new CookieOptions();
+            options.Path = "/";
+            options.Domain = "";
+            var password = "p-12345678";// + RandomNumberGenerator.GetInt32(200000000, 2000000000);
+            response.Cookies.Append("password", password, options);
+        }
+    }
+}
